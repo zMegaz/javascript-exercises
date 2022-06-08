638e60ae0716e1a744b846a91f5aeb53c70e84d9
Megaz <megaxz12@yahoo.com>
2022-06-08 11:04:57 +0400

    All tasks done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,8 @@
 const repeatString = function repeatString(str, num) {
-  str = "hey";
-  if (str != "" && num > 0) {
-    return str.repeat(num);
-  } else if (num < 0) {
+  if (num < 0) {
     return "ERROR";
-  } else if (str === "" && num === 10) {
-    return str;
   } else {
-    return "";
+    return str.repeat(num);
   }
 };
 
