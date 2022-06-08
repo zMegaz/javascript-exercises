16d7f054dfee5d3e0904a2610c597da3d2533e2a
Megaz <megaxz12@yahoo.com>
2022-06-08 10:46:59 +0400

    Blank string fail

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function repeatString(str, num) {
+  str = "hey";
+  if (str != "" && num > 0) {
+    return str.repeat(num);
+  } else if (num < 0) {
+    return "ERROR";
+  } else if (str === "" && num === 10) {
+    return str;
+  } else {
+    return "";
+  }
 };
 
 // Do not edit below this line
